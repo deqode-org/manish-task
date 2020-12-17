ac93ff9ee83dabf2abf0fa27aa49b18224dbed5b
Manish <mthakur@deqode.com>
2020-12-17 21:13:24 +0530

    Updates

---
+++ precache-manifest.ffbf8becd04ef2cd3b15578011ed6010.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d09477ff7a71507b71aa90cacfabefc9",
+    "url": "/manish-task/index.html"
+  },
+  {
+    "revision": "34460941d92e71c17d19",
+    "url": "/manish-task/static/css/main.a0cce386.chunk.css"
+  },
+  {
+    "revision": "0fed19e2cdae9b596338",
+    "url": "/manish-task/static/js/2.8a40a783.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/manish-task/static/js/2.8a40a783.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34460941d92e71c17d19",
+    "url": "/manish-task/static/js/main.f2aa1b1e.chunk.js"
+  },
+  {
+    "revision": "1ed2fcac53942a780e6c",
+    "url": "/manish-task/static/js/runtime-main.649da452.js"
+  }
+]);
